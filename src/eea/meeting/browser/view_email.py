2ace560496c8be36db7ec041b7ff4ca1a7549c2f
Irina Botez <irina.botez@eaudeweb.ro>
2016-09-09 17:40:32 +0300

    Optimized email archive
    *reduced the number of dictionary lists storing the sent emails

--- src/eea/meeting/browser/view_email.py
+++ src/eea/meeting/browser/view_email.py
@@ -5,16 +5,4 @@ class ViewSentEmails(BrowserView):
     email_archive = []
 
     def emails(self):
-
-        results = []
-
-        for email in self.email_archive:
-            results.append({
-                'sender': email['sender'],
-                'receiver': email['receiver'],
-                'cc': email['cc'],
-                'subject': email['subject'],
-                'body': email['body'],
-            })
-
-        return results
+        return self.email_archive
