aa32467bb2d43d4a2f6043a228ba72249b36cf26
Irina Botez <irina.botez@eaudeweb.ro>
2016-09-09 18:23:23 +0300

    Registered separate js file for Excel export

--- src/eea/meeting/browser/static/excel.js
+++ src/eea/meeting/browser/static/excel.js
@@ -6,7 +6,7 @@ function fnExcelReport() {
     tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
 
     tab_text = tab_text + "<table border='1px'>";
-    tab_text = tab_text + $('#myTable').html();
+    tab_text = tab_text + $('.display-email').html();
     tab_text = tab_text + '</table></body></html>';
 
     var data_type = 'data:application/vnd.ms-excel';
@@ -19,11 +19,11 @@ function fnExcelReport() {
             var blob = new Blob([tab_text], {
                 type: "application/csv;charset=utf-8;"
             });
-            navigator.msSaveBlob(blob, 'Test file.xls');
+            navigator.msSaveBlob(blob, 'Email_archive.xls');
         }
     } else {
-        $('#test').attr('href', data_type + ', ' + encodeURIComponent(tab_text));
-        $('#test').attr('download', 'Test file.xls');
+        $('#email_download').attr('href', data_type + ', ' + encodeURIComponent(tab_text));
+        $('#email_download').attr('download', 'Email_archive.xls');
     }
 
 }
