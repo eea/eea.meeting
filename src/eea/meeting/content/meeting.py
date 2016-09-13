f38ef7975205d0cb5778e1fa6665a8052ff55f4e
Irina Botez <irina.botez@eaudeweb.ro>
2016-09-13 18:18:21 +0300

    Added Emails container on meeting creation

--- src/eea/meeting/content/meeting.py
+++ src/eea/meeting/content/meeting.py
@@ -67,7 +67,13 @@ def on_save(obj, evt):
 def on_add(obj, evt):
     create_subscribers(obj)
 
+    create_emails(obj)
+
 
 def create_subscribers(container):
     createContentInContainer(container, 'eea.meeting.subscribers',
                              title='Subscribers', id='subscribers')
+
+def create_emails(container):
+    createContentInContainer(container, 'eea.meeting.emails',
+                             title='Emails', id='emails')
