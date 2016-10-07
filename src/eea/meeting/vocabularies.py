8165075d9b138fc4b344916c799bf4c4eb3bcab8
Irina Botez <irina.botez@eaudeweb.ro>
2016-10-07 10:56:51 +0300

    LDAP and local users search

--- src/eea/meeting/vocabularies.py
+++ src/eea/meeting/vocabularies.py
@@ -2,39 +2,76 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.interface import implements
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from plone import api
-from plone.app.ldap.ploneldap import util
+from zope.component import getMultiAdapter
+from Acquisition import aq_inner
 
+def search_user2(context, request, containing, criteria):
 
-
-def place_results(obj, results):
-    term_title = '{} | {} | {}'.format(obj.firstname + " " + obj.lastname, obj.uid, obj.email)
-
-    results.append(SimpleTerm(obj.email, title=term_title))
-
-def user_listing(substring, criteria):
     results = []
+
+    searchView = getMultiAdapter((aq_inner(context), request), name='pas_search')
+
+    if criteria[0] == 'Name':
+        ldap_field = 'cn'
+        regular_field = 'description'
+    elif criteria[0] == 'Email':
+        ldap_field = 'mail'
+        regular_field = 'email'
+    # elif criteria[0] == 'Organization':
+    #     ldap_field = 'o'
+    #     regular_field = None
+    elif criteria[0] == 'User ID':
+        ldap_field = 'uid'
+        regular_field = 'userid'
+
     # import pdb;pdb.set_trace()
-    # print util.getLDAPPlugin()['acl_users'].getUserNames()
+    ldapUsers = searchView.searchUsers(**{ldap_field: containing})
+    localUsers = searchView.searchUsers(**{regular_field: containing})
 
-    portal_catalog = api.portal.get_tool('portal_catalog')
-    brains = portal_catalog(portal_type="eea.meeting.subscriber")
+    # for field in [ldap_field, regular_field]:
+    #     explicit_users = searchView.searchUsers(**{field: containing})
+    #
+    import pdb;pdb.set_trace()
+    #
+    # for user in explicit_users:
+    #     if user['pluginid'] == 'ldap-plugin':
+    #         term_title = '{} | {} | {} | {}'.format(user['cn'], user['mail'], user['uid'])
+    #         results.append(SimpleTerm(user['mail'], title=term_title))
+    #     else:
+    #         term_title = '{} | {} | {} '.format(user['title'], user['email'], user['userid'])
+    #         results.append(SimpleTerm(user['email'], title=term_title))
+    #
+    # return results
 
-    for brain in brains:
-        user = brain.getObject()
 
-        if criteria[0] == 'Name':
-            if user.firstname.find(substring) > -1 or user.lastname.find(substring) > -1:
-                place_results(user, results)
-        elif criteria[0] == 'Email':
-            if user.email.find(substring) > -1:
-                place_results(user, results)
-        elif criteria[0] == 'Organization':
-            pass
-        elif criteria[0] == 'User ID':
-            if str(user.uid).find(substring) > -1:
-                place_results(user, results)
 
-    return results
+# def place_results(obj, results):
+#     term_title = '{} | {} | {}'.format(obj.firstname + " " + obj.lastname, obj.uid, obj.email)
+#
+#     results.append(SimpleTerm(obj.email, title=term_title))
+#
+# def user_listing(substring, criteria):
+#     results = []
+#
+#     portal_catalog = api.portal.get_tool('portal_catalog')
+#     brains = portal_catalog(portal_type="eea.meeting.subscriber")
+#
+#     for brain in brains:
+#         user = brain.getObject()
+#
+#         if criteria[0] == 'Name':
+#             if user.firstname.find(substring) > -1 or user.lastname.find(substring) > -1:
+#                 place_results(user, results)
+#         elif criteria[0] == 'Email':
+#             if user.email.find(substring) > -1:
+#                 place_results(user, results)
+#         elif criteria[0] == 'Organization':
+#             pass
+#         elif criteria[0] == 'User ID':
+#             if str(user.uid).find(substring) > -1:
+#                 place_results(user, results)
+#
+#     return results
 
 class LDAPListingVocabulary(object):
 
@@ -51,7 +88,30 @@ class LDAPListingVocabulary(object):
             vocab = []
 
         if context.REQUEST.get('search_user.buttons.search_user') is not None or context.REQUEST.get('search_user.buttons.addCC') is not None:
-            vocab = user_listing(containing, criteria)
+            # vocab = user_listing(containing, criteria)
+
+            vocab = search_user2(context, context.REQUEST, containing, criteria)
+
+            # searchView = getMultiAdapter((aq_inner(context), context.REQUEST), name='pas_search')
+            #
+            #
+            # explicit_users = [searchView.searchUsers(**{field: containing}) for field in ['cn']]
+            # import pdb;pdb.set_trace()
+
+
+
+
+
+
+
+            # users = UsersOverviewControlPanel(context, context.REQUEST)
+            # user_list = UsersOverviewControlPanel.doSearch(users, containing)
+            #
+            # for user in user_list:
+            #     place_results2(user, vocab)
+
+
+
 
 
         return SimpleVocabulary(vocab)
