e4259e19ef7d6262bca2fce4a03fe9e18e409632
Andrei Meliș <andrei.melis@eaudeweb.ro>
2016-10-12 17:50:35 +0300

    Safely add Memcached manager

--- src/eea/meeting/setuphandlers.py
+++ src/eea/meeting/setuphandlers.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+import logging
 from zope.component import queryAdapter
 from zope.component.hooks import getSite
 from plone.app.controlpanel.security import ISecuritySchema
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
+logger = logging.getLogger('eea.meeting')
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
@@ -26,15 +28,19 @@ def post_install(context):
 
     # Add memcached
     if 'MEMCache' not in site.objectIds():
-        oid = site.manage_addProduct[
-            'MemcachedManager'].manage_addMemcachedManager('MEMCache')
-        cache = site._getOb('MEMCache')
-        cache._settings['servers'] = ('memcached:11211',)
-        cache._p_changed = True
+        try:
+            oid = site.manage_addProduct[
+                'MemcachedManager'].manage_addMemcachedManager('MEMCache')
+        except Exception, err:
+            logger.exception(err)
+        else:
+            cache = site._getOb('MEMCache')
+            cache._settings['servers'] = ('memcached:11211',)
+            cache._p_changed = True
 
-        # Set cache for ldap-plugin
-        ldap_plugin = site['acl_users']['ldap-plugin']
-        ldap_plugin.ZCacheable_setManagerId(manager_id='MEMCache')
+            # Set cache for ldap-plugin
+            ldap_plugin = site['acl_users']['ldap-plugin']
+            ldap_plugin.ZCacheable_setManagerId(manager_id='MEMCache')
 
 def uninstall(context):
     """Uninstall script"""
