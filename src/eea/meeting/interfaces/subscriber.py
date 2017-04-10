f23d7e0f046d3d807de8e40b2e1fcb099097e155
Ghiță Bizău <ghita_bizau@yahoo.com>
2017-04-10 12:54:18 +0300

    [refs #83535] Use subscriber_roles vocabulary.

--- src/eea/meeting/interfaces/subscriber.py
+++ src/eea/meeting/interfaces/subscriber.py
@@ -5,26 +5,8 @@ from zope.interface import Interface
 
 from eea.meeting import _
 from eea.meeting.interfaces.util import validate_email
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from z3c.form.browser.radio import RadioFieldWidget
 from plone.autoform import directives
-
-
-# [TODO] Move in portal_vocabularies to be editable for East and South
-roles = SimpleVocabulary(
-    [SimpleTerm(
-        value=u'eni-seis-ii-south-nfp',
-        title=_(u'ENI-SEIS II South NFP')),
-     SimpleTerm(
-        value=u'medpol-nfp',
-        title=_(u'MedPOL NFP')),
-     SimpleTerm(
-        value=u'h2020-nfp',
-        title=_(u'H2020 NFP')),
-     SimpleTerm(
-        value=u'other',
-        title=_(u'Other'))]
-)
 
 
 class ISubscriber(Interface):
@@ -48,7 +30,7 @@ class ISubscriber(Interface):
 
     role = schema.Choice(
         title=_(u"Role"),
-        vocabulary=roles,
+        vocabulary="subscriber_roles",
         required=True,
     )
 
