b4373a31974f028b0c3e19ddd7dd141b8e776982
Irina Botez <irina.botez@eaudeweb.ro>
2017-04-12 12:53:27 +0300

    Updated Excel export function

--- src/eea/meeting/browser/static/excel.js
+++ src/eea/meeting/browser/static/excel.js
@@ -1,14 +1,28 @@
-$(document).ready(function() {
-  $("#btnExport").click(function(e) {
-    e.preventDefault();
+function fnExcelReport() {
+    var tab_text = '<html xmlns:x="urn:schemas-microsoft-com:office:excel">';
+    tab_text = tab_text + '<head><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>';
+
+    tab_text = tab_text + '<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions></x:ExcelWorksheet>';
+    tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
+
+    tab_text = tab_text + "<table border='1px'>";
+    tab_text = tab_text + $('#listing-table').html();
+    tab_text = tab_text + '</table></body></html>';
 
     var data_type = 'data:application/vnd.ms-excel';
-    var table_div = document.getElementById('table_wrapper');
-    var table_html = table_div.outerHTML.replace(/ /g, '%20');
 
-    var a = document.createElement('a');
-    a.href = data_type + ', ' + table_html;
-    a.download = 'email_archive.xls';
-    a.click();
-  });
-});
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        if (window.navigator.msSaveBlob) {
+            var blob = new Blob([tab_text], {
+                type: "application/csv;charset=utf-8;"
+            });
+            navigator.msSaveBlob(blob, 'email_archive.xls');
+        }
+    } else {
+        $('#btnExport').attr('href', data_type + ', ' + encodeURIComponent(tab_text));
+        $('#btnExport').attr('download', 'email_archive.xls');
+    }
+}
