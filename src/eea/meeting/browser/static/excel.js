a6f7a4652007f70bbe24c5f697a1d4d739e6bfdb
Irina Botez <irina.botez@eaudeweb.ro>
2017-04-18 11:10:48 +0300

    Code cleanup

--- src/eea/meeting/browser/static/excel.js
+++ src/eea/meeting/browser/static/excel.js
@@ -15,22 +15,21 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
         var row = "";
 
         for (var index in arrData[0]) {
-
-            //Now convert each value to string and comma-seprated
+            //convert each value to string and comma-seprated
             row += index + ',';
         }
 
         row = row.slice(0, -1);
 
-        //append Label row with line break
+        //append Label row
         CSV += row + '\r\n';
     }
 
-    //1st loop is to extract each row
+    //extract each row
     for (var i = 0; i < arrData.length; i++) {
         var row = "";
 
-        //2nd loop will extract each column and convert it in string comma-seprated
+        //extract each column and convert it in string comma-seprated
         for (var index in arrData[i]) {
             row += '"' + arrData[i][index] + '",';
         }
@@ -48,26 +47,19 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
 
     //Generate a file name
     var fileName = "Export_";
-    //this will remove the blank-spaces from the title and replace it with an underscore
+    //remove the blank-spaces from the title and replace it with an underscore
     fileName += ReportTitle.replace(/ /g,"_");
 
-    //Initialize file format you want csv or xls
+    //Initialize file format
     var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
 
-    // Now the little tricky part.
-    // you can use either>> window.open(uri);
-    // but this will not work in some browsers
-    // or you will not get the correct file extension
-
-    //this trick will generate a temp <a /> tag
     var link = document.createElement("a");
     link.href = uri;
 
-    //set the visibility hidden so it will not effect on your web-layout
     link.style = "visibility:hidden";
     link.download = fileName + ".csv";
 
-    //this part will append the anchor tag and remove it after automatic click
+    //remove the temporary anchor after clicking it
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
