cf02f305cc9e97a49389948cabbcf12c9b5d4841
Irina Botez <irina.botez@eaudeweb.ro>
2017-09-19 17:10:54 +0300

    Excel export function update

--- src/eea/meeting/browser/static/excel.js
+++ src/eea/meeting/browser/static/excel.js
@@ -1,68 +1,17 @@
-function export_excel(selector, title, json_opts)
-{
-  var table = $(selector).tableToJSON(json_opts);
-  JSONToCSVConvertor(table, title, true);
-}
+window.saveFile = function saveFile (selector, title, json_opts) {
+    var table = $('#subscribers').tableToJSON({ignoreColumns: [0]});
 
-
-function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
-    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
-    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-
-    for(i in arrData)
-    {   delete arrData[i]['Address'];
-        delete arrData[i]['Phone'];
-        delete arrData[i]['Reimbursed'];
-        delete arrData[i]['State'];
-        delete arrData[i]['User Name'];
-        delete arrData[i]['Visa'];
-        delete arrData[i]['Role'];
-    }
-
-    var CSV = '';
-
-    if (ShowLabel) {
-        var row = "";
-
-        for (var index in arrData[0]) {
-            //convert each value to string and comma-seprated
-            row += index + ',';
-        }
-
-        row = row.slice(0, -1);
-
-        //append Label row
-        CSV += row + '\r\n';
-    }
-
-    //extract each row
-    for (var i = 0; i < arrData.length; i++) {
-        var row = "";
-
-        //extract each column and convert it in string comma-seprated
-        for (var index in arrData[i]) {
-            row += '"' + arrData[i][index] + '",';
-        }
-
-        row.slice(0, row.length - 1);
-
-        //add a line break after each row
-        CSV += row + '\r\n';
-    }
-
-    if (CSV == '') {
-        alert("Invalid data");
-        return;
-    }
-
-    //Generate a file name
-    var fileName = "Export_";
-    //remove the blank-spaces from the title and replace it with an underscore
-    fileName += ReportTitle.replace(/ /g,"_");
-
-    var blob = new Blob([CSV], {
-      type: 'application/vnd.ms-excel;charset=charset=utf-8'
+    table.forEach(function(item){
+        delete item['Address'];
+        delete item['Reimbursed'];
+        delete item['State'];
+        delete item['User Name'];
+        delete item['Visa'];
+        delete item['Role'];
     });
 
-    saveAs(blob, fileName + ".xlsx");
+
+    var opts = [{sheetid:'Sheet 1',header:true}];
+    var res = alasql('SELECT * INTO XLSX("Export_'+title+'.xls",?) FROM ?',
+                     [opts,[table]]);
 }
