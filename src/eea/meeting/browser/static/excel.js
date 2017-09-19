af4fe500ab7bf5d2eccb795a2b6646ffa43e2efb
Irina Botez <irina.botez@eaudeweb.ro>
2017-09-19 17:30:12 +0300

    Email archive export update

--- src/eea/meeting/browser/static/excel.js
+++ src/eea/meeting/browser/static/excel.js
@@ -1,15 +1,17 @@
 window.saveFile = function saveFile (selector, title, json_opts) {
-    var table = $('#subscribers').tableToJSON({ignoreColumns: [0]});
+    var table = $(selector).tableToJSON({ignoreColumns: [0]});
 
-    table.forEach(function(item){
-        delete item['Address'];
-        delete item['Reimbursed'];
-        delete item['State'];
-        delete item['User Name'];
-        delete item['Visa'];
-        delete item['Role'];
-    });
 
+    if(selector == '#subscribers'){
+        table.forEach(function(item){
+            delete item['Address'];
+            delete item['Reimbursed'];
+            delete item['State'];
+            delete item['User Name'];
+            delete item['Visa'];
+            delete item['Role'];
+        });
+    }
 
     var opts = [{sheetid:'Sheet 1',header:true}];
     var res = alasql('SELECT * INTO XLSX("Export_'+title+'.xls",?) FROM ?',
