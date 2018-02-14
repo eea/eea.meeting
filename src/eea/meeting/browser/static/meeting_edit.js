f10b03f0595e95e064d69af46e2398a027277e43
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-02-14 12:58:21 +0200

    [refs #92256] Webminar: WIP behavior on meeting edit.

---
+++ src/eea/meeting/browser/static/meeting_edit.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var body = $('body');
+  if($(body).hasClass("portaltype-eea-meeting") && $(body).hasClass("template-edit")) {
+    alert("WIP meeting_edit");
+  }
+});
