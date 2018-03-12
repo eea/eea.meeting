28b78e364b8abde797d32c7af0552ac67e7b37fe
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-03-12 13:03:08 +0200

    [refs #92650] Add human_readable_date: fix wrong start and end dates (using the self.timezone).

--- src/eea/meeting/content/meeting.py
+++ src/eea/meeting/content/meeting.py
@@ -54,6 +54,38 @@ class Meeting(Container):
             roles_dict[key] = vocab[key].title
         return roles_dict
 
+    @property
+    def human_readable_date(self):
+        """ Return start date - end date in format:
+            10 September - 1 October 2018
+            3-5 August 2017
+
+            Solves timezone problem - wrong start or end day
+            self.start
+                datetime.datetime(2020, 9, 21, 22, 0, tzinfo=<UTC>)
+            self.start.astimezone(timezone)
+                datetime.datetime(2020, 9, 22, 0, 0,
+                tzinfo=<DstTzInfo 'Europe/Copenhagen' CEST+2:00:00 DST>)
+        """
+        timezone = pytz.timezone(self.timezone)
+        # We use the timezone set for each item instead of website's one
+        # site_timezone = pytz.timezone(default_timezone())
+        # else the dates are wrong again. When an event is listed we
+        # want to show the dates as saved, ignoring hours and timezone.
+        start_date = self.start.astimezone(timezone)
+        end_date = self.end.astimezone(timezone)
+        start_day = start_date.day
+        start_month = start_date.strftime("%B")
+        end_day = end_date.day
+        end_month = end_date.strftime("%B")
+
+        if start_month == end_month:
+            if start_day == end_day:
+                return "%s %s" % (start_day, start_month)
+            return "%s-%s %s" % (start_day, end_day, start_month)
+
+        return "%s %s - %s %s" % (start_day, start_month, end_day, end_month)
+
 
 def on_save(obj, evt):
     # This triggers also on the container creation, not only on save props!
