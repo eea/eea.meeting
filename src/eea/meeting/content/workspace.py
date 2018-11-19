0571dfd2973a489f597a4ea22323c8b94774f83e
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-11-19 18:03:50 +0200

    [refs #99955] Meeting workspace: WIP local roles.

--- src/eea/meeting/content/workspace.py
+++ src/eea/meeting/content/workspace.py
@@ -6,3 +6,12 @@ from eea.meeting.interfaces import IMeetingWorkspace
 @implementer(IMeetingWorkspace)
 class MeetingWorkspace(Container):
     """ EEA Meeting Workspace content type"""
+    # __ac_local_roles_block__ = True
+    #
+    # def get_meeting(self):
+    #     return self.aq_parent
+    #
+    # @property
+    # def __ac_local_roles__(self):
+    #     import pdb; pdb.set_trace()
+    #     return {}
