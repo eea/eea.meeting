4b8b4861f44c2ad7c190131151fd031ff61cfdb2
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-11-20 12:25:51 +0200

    [refs #99955] Meeting workspace: WIP local roles.

--- src/eea/meeting/content/workspace.py
+++ src/eea/meeting/content/workspace.py
@@ -1,17 +1,24 @@
-from zope.interface import implementer
-from plone.dexterity.content import Container
 from eea.meeting.interfaces import IMeetingWorkspace
+from plone.dexterity.content import Container
+from zope.globalrequest import getRequest
+from zope.interface import implementer
 
 
 @implementer(IMeetingWorkspace)
 class MeetingWorkspace(Container):
     """ EEA Meeting Workspace content type"""
-    # __ac_local_roles_block__ = True
-    #
-    # def get_meeting(self):
-    #     return self.aq_parent
-    #
-    # @property
-    # def __ac_local_roles__(self):
-    #     import pdb; pdb.set_trace()
-    #     return {}
+    @property
+    def __ac_local_roles__(self):
+        """ Manage custom roles for specific cases
+            This container and its child items should be accessed only by
+            meeting members (subscribers)
+        """
+        # This code runs for this container and also for all its child items
+        request = getRequest()
+        workspace = [
+            x for x in request.PARENTS[:-1]
+            if x.portal_type == 'eea.meeting.workspace'
+            ][0]
+        meeting = workspace.aq_parent
+        import pdb; pdb.set_trace()
+        return {}
