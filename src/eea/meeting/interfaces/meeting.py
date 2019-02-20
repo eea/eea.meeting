59562a2f41d97256a89e39215b702e8a78e4f68a
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-02-20 12:17:57 +0200

    [refs #97529] Add min and max values for From and To fields in meeting edit form.

--- src/eea/meeting/interfaces/meeting.py
+++ src/eea/meeting/interfaces/meeting.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from eea.meeting import _
 from eea.meeting.interfaces.util import validate_email
 from plone.app.textfield import RichText
@@ -7,6 +6,7 @@ from zope import schema
 from zope.interface import Interface
 from zope.interface import invariant, Invalid
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+import datetime
 
 
 meeting_types = SimpleVocabulary(
@@ -51,12 +51,16 @@ class IMeeting(Interface):
         title=_(u"From"),
         description=_(u"Allow registration starting with this datetime."),
         required=False,
+        min=datetime.datetime(2018, 1, 1),
+        max=datetime.datetime(datetime.datetime.now().year + 10, 12, 31)
     )
 
     allow_register_end = schema.Datetime(
         title=_(u"To"),
         description=_(u"Allow registration until this datetime."),
         required=False,
+        min=datetime.datetime(2018, 1, 1),
+        max=datetime.datetime(datetime.datetime.now().year + 10, 12, 31)
     )
 
     restrict_content_access = schema.Bool(
