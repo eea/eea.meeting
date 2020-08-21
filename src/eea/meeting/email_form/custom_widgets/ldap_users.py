400a6c03d93362e09ab842feb6d292b5116a1271
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-08-21 11:58:39 +0300

    [refs #116841] Fix jenkins errors: ldap.

--- src/eea/meeting/email_form/custom_widgets/ldap_users.py
+++ src/eea/meeting/email_form/custom_widgets/ldap_users.py
@@ -1,3 +1,4 @@
+""" LDAP Users """
 from eea.meeting.interfaces import ISearchUser
 from z3c.form import field
 from z3c.form import group
@@ -6,11 +7,13 @@ from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile as PTF
 
 
 class ResultsGroup(group.Group):
+    """ Results Group """
     label = u'LDAP Users'
     fields = field.Fields(ISearchUser).select('results')
 
     # fields['results'].widgetFactory = CheckBoxFieldWidget
 
     def updateWidgets(self, prefix=None):
+        """ update widgets """
         super(ResultsGroup, self).updateWidgets(prefix)
         self.widgets['results'].template = PTF('widgets/ldap_users.pt')
