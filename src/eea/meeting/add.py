17c917673b12ec00f8f9cbbbafcdb89f167b91fb
Ghiță Bizău <ghita_bizau@yahoo.com>
2020-08-21 12:00:54 +0300

    [refs #116841] Fix jenkins errors: add.

--- src/eea/meeting/add.py
+++ src/eea/meeting/add.py
@@ -1,10 +1,13 @@
+""" Add """
 from plone.dexterity.utils import createContentInContainer
 
 
 def handler(obj, evt):
+    """ handler """
     create_subscribers(obj)
 
 
 def create_subscribers(container):
+    """ create subscribers """
     createContentInContainer(container, 'eea.meeting.subscribers',
                              title='Subscribers', id='subscribers')
