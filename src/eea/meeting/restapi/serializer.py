f565f53b0063e0fc89b2e118de76cb8f018ec68e
ionlizarazu <ilizarazu@codesyntax.com>
2022-01-24 16:48:10 +0100

    reorganize anonymous registration responses

--- src/eea/meeting/restapi/serializer.py
+++ src/eea/meeting/restapi/serializer.py
@@ -11,7 +11,9 @@ from AccessControl import getSecurityManager
 @adapter(IMeeting, Interface)
 class SerializerToJsonMeeting(SerializeToJson):
     def __call__(self, version=None, include_items=True):
-        result = super(SerializerToJsonMeeting, self).__call__(version, include_items)
+        result = super(SerializerToJsonMeeting, self).__call__(
+            version, include_items
+        )
         subscribers = self.context.get("subscribers")
         emails = self.context.get("emails")
         sm = getSecurityManager()
@@ -30,9 +32,24 @@ class SerializerToJsonMeeting(SerializeToJson):
                 {"portal_type": "AnonymousForm", "review_state": "published"}
             )
             if anonymousforms_list:
-                result["anonymous_registration_form_url"] = anonymousforms_list[
-                    0
-                ].getURL()
+                result["anonymous_registration_form"] = {
+                    "url": anonymousforms_list[0].getURL(),
+                    "published": True,
+                }
+            else:
+                anonymousforms_list = self.context.getFolderContents(
+                    {"portal_type": "AnonymousForm", "review_state": "private"}
+                )
+                if anonymousforms_list:
+                    result["anonymous_registration_form"] = {
+                        "url": anonymousforms_list[0].getURL(),
+                        "published": False,
+                    }
+
+        result["registrations_open"] = False
+        if self.context.registrations_open():
+            result["registrations_open"] = True
 
         result["is_registered"] = self.context.is_registered()
+        result["is_folderish"] = True
         return result
