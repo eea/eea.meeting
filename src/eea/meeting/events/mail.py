55b6c7a8bb1f680507d23584e56b2a9b89949956
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-03-27 12:17:50 +0200

    send emails in HTML format

--- src/eea/meeting/events/mail.py
+++ src/eea/meeting/events/mail.py
@@ -1,6 +1,8 @@
 """ Mail """
 # -*- coding: utf-8 -*-
 import logging
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from smtplib import SMTPException
 
 from Acquisition import aq_inner
@@ -17,6 +19,7 @@ from plone.registry.interfaces import IRegistry
 from plone.stringinterp.interfaces import IStringInterpolator
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces.controlpanel import IMailSchema
+from Products.CMFPlone.utils import safe_text
 from Products.MailHost.MailHost import MailHostError
 from Products.statusmessages.interfaces import IStatusMessage
 from zope.component import adapter, getUtility
@@ -139,23 +142,26 @@ class CustomMailActionExecutor(MailActionExecutor):
                 if request:
                     messages = IStatusMessage(request)
                     msg = _(
-                        "Error sending email from content rule. You must "
-                        "provide a source address for mail "
-                        "actions or enter an email in the portal properties"
+                        u"Error sending email from content rule. You must "
+                        u"provide a source address for mail "
+                        u"actions or enter an email in the portal properties"
                     )
-                    messages.add(msg, type="error")
+                    messages.add(msg, type=u"error")
                 return False
 
             from_name = self.mail_settings.email_from_name.strip('"')
-            source = f'"{from_name}" <{from_address}>'
+            source = '"{0}" <{1}>'.format(from_name, from_address)
 
         recip_string = interpolator(self.element.recipients)
         if recip_string:  # check recipient is not None or empty string
-            recipients = {
-                str(mail.strip())
-                for mail in recip_string.split(",")
-                if mail.strip()
-            }
+            # pylint: disable=consider-using-set-comprehension
+            recipients = set(
+                [
+                    str(mail.strip())
+                    for mail in recip_string.split(",")
+                    if mail.strip()
+                ]
+            )
         else:
             recipients = set()
 
@@ -169,20 +175,34 @@ class CustomMailActionExecutor(MailActionExecutor):
 
         # prepend interpolated message with \n to avoid interpretation
         # of first line as header
-        message = f"\n{interpolator(self.element.message)}"
+        message = u"\n{0}".format(interpolator(self.element.message))
 
         subject = interpolator(self.element.subject)
 
         for email_recipient in recipients:
+            msgRoot = MIMEMultipart("related")
+            msgRoot["Subject"] = subject
+            msgRoot["From"] = source
+            msgRoot["To"] = email_recipient
+            msgRoot.preamble = "This is a multi-part message in MIME format"
+
+            msgAlternative = MIMEMultipart("alternative")
+            msgRoot.attach(msgAlternative)
+
+            msgText = MIMEText("This is the alternative plain text message.")
+            msgAlternative.attach(msgText)
+
+            msgHTML = MIMEText(safe_text(message, "utf-8"), "html")
+            msgAlternative.attach(msgHTML)
             try:
-                # XXX: We're using "immediate=True" because otherwise we won't
+                # We're using "immediateTrue" because otherwise we won't
                 # be able to catch SMTPException as the smtp connection is made
                 # as part of the transaction apparatus.
                 # AlecM thinks this wouldn't be a problem if mail queuing was
                 # always on -- but it isn't. (stevem)
                 # so we test if queue is not on to set immediate
                 mailhost.send(
-                    message,
+                    msgRoot.as_string(),
                     email_recipient,
                     source,
                     subject=subject,
